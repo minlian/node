683060c780f02e373d6ecb8242536f2bfdd6208e
minlian <minlian666@163.com>
2019-12-24 18:45:02 +0800

    链表测试的工具类

--- ListNode.java
+++ ListNode.java
@@ -15,7 +15,7 @@ public class ListNode {
     public ListNode(int[] arr){
 
         if(arr == null || arr.length == 0)
-            throw new IllegalArgumentException("arr can not be empty");
+            throw new IllegalArgumentException("arr can not be empty ！");
 
         this.val = arr[0];
         ListNode cur = this;
